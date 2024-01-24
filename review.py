846ed060691b7d336da916508d238144ca8d824c
Michael J. Johnson Jr. <mjohnson12279@gmail.com>
2024-01-24 15:01:17 +0000

    Printing our review csv's

--- review.py
+++ review.py
@@ -1,6 +1,11 @@
 from openai import OpenAI
+import pandas as pd
 
-client = OpenAI(
-  organization='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',
-)
+# client = OpenAI(
+#   organization='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',
+# )
 
+df = pd.read_csv("one_star_reviews_sample.csv")
+print(df.to_string())
+md = pd.read_csv("four_star_reviews_sample.csv")
+print(md.to_string())
