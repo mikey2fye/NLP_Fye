4f3be982d7f09bf06a6053c4f63aeb701ec0e11d
Michael J. Johnson Jr. <mjohnson12279@gmail.com>
2024-01-29 07:41:58 +0000

    yup.

--- review.py
+++ review.py
@@ -1,11 +1,27 @@
 from openai import OpenAI
+import os
 import pandas as pd
+import csv
+import json
 
-# client = OpenAI(
-#   organization='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',
-# )
+def parse_reviews(file):
+  df = pd.read_csv(file)
+  count = 1
+  for review in df:
+    if count < 6:
+      prompt = f"Given the followingt review for a restaurant: {review}. What is the sentiment of this review?"
+      response = client.completions.create(model = "gpt-3.5-turbo", 
+        prompt = prompt,
+        temperature = 0.5,
+        max_tokens = 100
+    )
+    count += 1
+  return response.choices[0].text.strip()
 
-df = pd.read_csv("one_star_reviews_sample.csv")
-print(df.to_string())
+client = OpenAI(api_key = "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")
+
+# print(df.to_string())
 md = pd.read_csv("four_star_reviews_sample.csv")
-print(md.to_string())
+# print(md.to_string())
+
+print(parse_reviews("one_star_reviews_sample.csv"))
