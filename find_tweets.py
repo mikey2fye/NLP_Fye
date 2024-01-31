66dae0d390af860f41b04c2d5c7f53c8091847a3
Michael J. Johnson Jr. <mjohnson12279@gmail.com>
2024-01-31 15:06:42 +0000

    frfr this time

--- find_tweets.py
+++ find_tweets.py
@@ -4,7 +4,7 @@ buzzwords = ['Trump', "President", "Joe Biden", "Donald Trump"]
 t = open("presidential_debate_tweets.txt", "w")
 with open("presidential_debate.txt", "r") as f:
     for line in f.readlines():
-        x = re.findall(buzzwords, line)
+        x = re.findall("Trump", line)
         if len(x) != 0:
             t.write(line)
 t.close()
