59719da38d56e969de5203b68967c3adc7a7e523
Michael J. Johnson Jr. <mjohnson12279@gmail.com>
2024-01-31 14:38:53 +0000

    Starting Assignment 2

--- review.py
+++ review.py
@@ -7,7 +7,7 @@ import json
 def parse_reviews(file):
   df = pd.read_csv(file)
   count = 1
-  for review in df:
+  for review in df["Reviews"]:
     if count < 6:
       prompt = f"Given the followingt review for a restaurant: {review}. What is the sentiment of this review?"
       response = client.completions.create(model = "gpt-3.5-turbo", 
@@ -18,8 +18,12 @@ def parse_reviews(file):
     count += 1
   return response.choices[0].text.strip()
 
-client = OpenAI(api_key = "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")
+client = OpenAI(api_key = "(insert api key here)")
 
+# chat_completion = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[{"role": "user", "content": "Hello world"}]
+# )
 # print(df.to_string())
 md = pd.read_csv("four_star_reviews_sample.csv")
 # print(md.to_string())
