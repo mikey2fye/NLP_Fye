f2ffc2c86467836c2dd339fbb368ca4dcdbac4de
Michael J. Johnson Jr. <mjohnson12279@gmail.com>
2024-02-05 03:30:23 +0000

    Updated

--- find_tweets.py
+++ find_tweets.py
@@ -4,7 +4,8 @@ def find_all_tweets(file):
     pass
 
 def find_pres_tweets(file): 
-    buzzwords = ["Trump", "@donaldtrump", "President", "DonaldTrump", "Joe Biden", "Donald Trump", "Chris Wallace", "Biden", "Joe", "JoeBiden," "@joebiden"]
+    tweets = 0
+    buzzwords = ["Trump", "@donaldtrump", "President", "DonaldTrump", "Joe Biden", "Donald Trump", "Chris Wallace", "ChrisWallace", "@chriswallace", "Biden", "Joe", "JoeBiden," "@joebiden"]
     t = open(file, "w")
     with open("presidential_debate.txt", "r") as f:
         for line in f.readlines():
@@ -13,13 +14,15 @@ def find_pres_tweets(file):
                 x = re.findall(buzzwords[i], line)
                 if len(x) != 0:
                     t.write(line)
+                    tweets += 1
                     break
                 i += 1
     t.close()
     f.close()
+    return "There are " + str(tweets) + " tweets about the presidential debate that reference Joe Biden, Donald Trump, and Chris Walllace."
 
 
 # find_all_tweets("all_tweets.txt")
-find_pres_tweets("presidential_debate_tweets.txt")
+print(find_pres_tweets("presidential_debate_tweets.txt"))
 
     
