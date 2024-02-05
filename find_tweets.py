3f79d5870d3cadc1f879dc8b3bef1c6715092aa7
Michael J. Johnson Jr. <mjohnson12279@gmail.com>
2024-02-05 02:37:31 +0000

    Added buzzwords

--- find_tweets.py
+++ find_tweets.py
@@ -1,18 +1,25 @@
 import re
 
-buzzwords = ["Trump", "President", "Joe Biden", "Donald Trump", "Chris Wallace", "Biden", "Joe", "JoeBiden," "@joebiden"]
-t = open("presidential_debate_tweets.txt", "w")
-with open("presidential_debate.txt", "r") as f:
-    for line in f.readlines():
-        i = 0
-        while i < len(buzzwords):
-            x = re.findall(buzzwords[0], line)
-            if len(x) != 0:
-                t.write(line)
-                break
-            i += 1
+def find_all_tweets(file):
+    pass
 
-t.close()
-f.close()
+def find_pres_tweets(file): 
+    buzzwords = ["Trump", "@donaldtrump", "President", "DonaldTrump", "Joe Biden", "Donald Trump", "Chris Wallace", "Biden", "Joe", "JoeBiden," "@joebiden"]
+    t = open(file, "w")
+    with open("presidential_debate.txt", "r") as f:
+        for line in f.readlines():
+            i = 0
+            while i < len(buzzwords):
+                x = re.findall(buzzwords[i], line)
+                if len(x) != 0:
+                    t.write(line)
+                    break
+                i += 1
+    t.close()
+    f.close()
+
+
+# find_all_tweets("all_tweets.txt")
+find_pres_tweets("presidential_debate_tweets.txt")
 
     
